5db1835d2a42e73bcdcdf3ac2b960891096f9721
Alicia Skutle <alicia.skutle@gmail.com>
2019-06-04 11:38:02 +0200

    Modified contact form

--- scripts/contact.js
+++ scripts/contact.js
@@ -2,6 +2,7 @@
 var firstName = document.getElementById("firstName");
 var lastName = document.getElementById("lastName");
 var email = document.getElementById("email");
+var message = document.getElementById("message");
 
 //RegEx for email and phone
 var emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
@@ -11,6 +12,8 @@ var phoneRegex = /^(\d{3}[\- .]\d{3}[\- .]\d{4})$/;
 firstName.addEventListener("blur", function() {
   if (firstName.value === "") {
     document.getElementById("firstNameError").style.display = "block";
+  } else {
+    document.getElementById("firstNameError").style.display = "none";
   }
 });
 
@@ -18,6 +21,8 @@ firstName.addEventListener("blur", function() {
 lastName.addEventListener("blur", function() {
   if (lastName.value === "") {
     document.getElementById("lastNameError").style.display = "block";
+  } else {
+    document.getElementById("lastNameError").style.display = "none";
   }
 });
 
@@ -32,5 +37,14 @@ email.addEventListener("blur", function() {
   } else {
     document.getElementById("emailError").style.display = "none";
     return true;
+  }
+});
+
+//Validate message
+message.addEventListener("blur", function() {
+  if (message.value === "") {
+    document.getElementById("messageError").style.display = "block";
+  } else {
+    document.getElementById("messageError").style.display = "none";
   }
 });
