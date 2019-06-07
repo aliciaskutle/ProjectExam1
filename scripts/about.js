05f5841ee76ed9f7747203ceefd9f86b97bd9517
Alicia Skutle <alicia.skutle@gmail.com>
2019-06-07 22:49:32 +0200

    Modified slideshow on about page

--- scripts/about.js
+++ scripts/about.js
@@ -1,34 +1,45 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var images = document.querySelectorAll(".slideshow-images");
+var imageCount = images.length;
+var nextImage = document.querySelector(".next");
+var previousImage = document.querySelector(".previous");
+var count = 0;
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+function displayNextImage() {
+  images[count].classList.remove("active-image");
+
+  if (count < imageCount - 1) {
+    count++;
+  } else {
+    count = 0;
+  }
+
+  images[count].classList.add("active-image");
+  console.log(count);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
+function displayPreviousImage() {
+  images[count].classList.remove("active-image");
+
+  if (count > 0) {
+    count--;
+  } else {
+    count = imageCount - 1;
+  }
+
+  images[count].classList.add("active-image");
+  console.log(count);
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {
-    slideIndex = 1;
+function keyPress(e) {
+  e = e || window.event;
+
+  if (e.keyCode == "37") {
+    displayPreviousImage();
+  } else if (e.keyCode == "39") {
+    displayNextImage();
   }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
 }
+
+nextImage.addEventListener("click", displayNextImage);
+previousImage.addEventListener("click", displayPreviousImage);
+document.addEventListener("keydown", keyPress);
