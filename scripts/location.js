37cad3fd2497c2e95076a5006c3f26e84705ada3
Alicia Skutle <alicia.skutle@gmail.com>
2019-06-11 21:13:32 +0200

    Optimized ISS tracking

--- scripts/location.js
+++ scripts/location.js
@@ -1,20 +1,41 @@
+function fetchLocation() {
+  return fetch("http://api.open-notify.org/iss-now.json").then(response =>
+    response.json()
+  );
+}
+
+var markers = [];
+
+function clearMarkers() {
+  markers.forEach(marker => marker.setMap(null));
+}
+
 var map;
 function initMap() {
-  fetch("http://api.open-notify.org/iss-now.json")
-    .then(response => response.json())
-    .then(location => {
-      console.log(location);
-      var position = {
-        lat: parseFloat(location.iss_position.latitude),
-        lng: parseFloat(location.iss_position.longitude)
-      };
-      map = new google.maps.Map(document.getElementById("map"), {
-        center: position,
-        zoom: 3
-      });
-      var marker = new google.maps.Marker({
-        position: position,
-        map: map
-      });
+  fetchLocation().then(location => {
+    var position = {
+      lat: parseFloat(location.iss_position.latitude),
+      lng: parseFloat(location.iss_position.longitude)
+    };
+
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: position,
+      zoom: 3
     });
+
+    setInterval(() => {
+      fetchLocation().then(location => {
+        clearMarkers();
+        var position = {
+          lat: parseFloat(location.iss_position.latitude),
+          lng: parseFloat(location.iss_position.longitude)
+        };
+        var marker = new google.maps.Marker({
+          position: position,
+          map: map
+        });
+        markers.push(marker);
+      });
+    }, 1000);
+  });
 }
